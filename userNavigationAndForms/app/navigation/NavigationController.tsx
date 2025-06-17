6ed50041e28cd42d154a7a0950291b83e6abf6e9
Abil <abilmomin6@gmail.com>
2025-06-17 01:05:07 -0600

    Cleaning Up Code

--- userNavigationAndForms/app/navigation/NavigationController.tsx
+++ userNavigationAndForms/app/navigation/NavigationController.tsx
@@ -1,20 +1,34 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Calgary from '../pages/Calgary';
-import Vancouver from '../pages/Vancouver';
-import SignUp from '../pages/SignUp';
+import { Ionicons } from '@expo/vector-icons';
+import CalgaryScreen from '../pages/Calgary';
+import VancouverScreen from '../pages/Vancouver';
 
 const Tab = createBottomTabNavigator();
 
-const NavigationController: React.FC = () => (
-  <NavigationContainer>
-    <Tab.Navigator>
-      <Tab.Screen name="Calgary" component={Calgary} />
-      <Tab.Screen name="Vancouver" component={Vancouver} />
-      <Tab.Screen name="SignUp" component={SignUp} />
+const NavigationController: React.FC = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }: { route: { name: string } }) => ({
+        tabBarIcon: ({ focused, color, size }: { focused: boolean; color: string; size: number }) => {
+          let iconName: keyof typeof Ionicons.glyphMap;
+          if (route.name === 'Calgary') {
+            iconName = focused ? 'location' : 'location-outline';
+          } else if (route.name === 'Vancouver') {
+            iconName = focused ? 'location' : 'location-outline';
+          } else {
+            iconName = 'location-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#007AFF',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
+      <Tab.Screen name="Calgary" component={CalgaryScreen} />
+      <Tab.Screen name="Vancouver" component={VancouverScreen} />
     </Tab.Navigator>
-  </NavigationContainer>
-);
+  );
+};
 
 export default NavigationController;
